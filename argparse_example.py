78f94cc461b5c8d0ee9c0e4389ce6df2da3d06da
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-07-22 23:57:36 +0530

    Update argparse_example.py

--- argparse_example.py
+++ argparse_example.py
@@ -4,9 +4,11 @@ ap = argparse.ArgumentParser()
 
 ap.add_argument("-n", "--name", required = True, help = "enter your name")
 ap.add_argument("-s", "--surname", required = True, help = "enter your surname")
-ap.add_argument("-a", "--age", required = True, help = "enter your age")
+ap.add_argument("-a", "--age", help = "enter your age")
 ap.add_argument("-c", "--country", required = True, help = "enter your country") 
 
 args = vars(ap.parse_args())
 
-print("Name: ", args["name"])
+print("Name: ", args["name"], args["surname"])
+print("Age: ", args["age"])
+print("Country: ", args["country"])
