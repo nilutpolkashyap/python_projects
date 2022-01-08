c4c53041ff8e09bc82dc20989b9ecfccfe59953f
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2022-01-08 23:50:14 +0530

    Add files via upload

---
+++ open_weather_map_api/forecast_api_call.py
@@ -0,0 +1,40 @@
+import requests, json
+ 
+api_key = "253126090068251f3ba4898c83f65742"
+ 
+base_url = "http://api.openweathermap.org/data/2.5/forecast?"
+ 
+city_name = input("Enter city name : ")
+ 
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+ 
+response = requests.get(complete_url)
+ 
+x = response.json()
+ 
+if x["cod"] != "404":
+ 
+    y = x["list"]
+    z = y[0]['main']
+
+    print("Max Temp : ", z['temp_max'])
+    print("Min Temp : ", z['temp_min'])
+ 
+    # current_temperature = y["temp"]
+    # current_temperature = current_temperature - 273
+ 
+    # current_pressure = y["pressure"]
+ 
+    # current_humidity = y["humidity"]
+ 
+    # z = x["wind"]
+ 
+    # wind_speed = z["speed"]
+ 
+    # print(" Temperature (in degree Celsius) = " + str(current_temperature) +
+    #       "\n Atmospheric Pressure (in hPa unit) = " + str(current_pressure) +
+    #       "\n Humidity (in percentage) = " + str(current_humidity) +
+    #       "\n Wind Speed (in meter/seconds) = " +  str(wind_speed))
+ 
+else:
+    print(" City Not Found ")
