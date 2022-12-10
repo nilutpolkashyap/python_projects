af97f46dcc2faf14fbcc81178b65770f1f3ab998
nilutpolkashyap <nkelectronicshlnp@gmail.com>
2022-12-10 15:10:41 +0530

    example for python pytest

--- pytest_example/greeting/tests/test_greeting.py
+++ pytest_example/greeting/tests/test_greeting.py
@@ -6,3 +6,7 @@ class Test_Greeting:
     def test_greet(self):
         greet = greeting('Nilutpol')
         assert greet == 'Hello Nilutpol'
+
+    def test_greet2(self):
+        greet = greeting('You')
+        assert greet == 'Hello You'
